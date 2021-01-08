cf565e366c481e7bbb0cc3ed8aeadef6e173bad2
Yuu6883 <okumura6883@gmail.com>
2021-01-08 10:46:11 -0800

    Options update

--- src/index.js
+++ src/index.js
@@ -10,19 +10,19 @@ const server = new Server();
 const engine = server.game.engine;
 
 engine.setOptions({
-    VIRUS_COUNT: 20,
+    VIRUS_COUNT: 100,
     PLAYER_MAX_CELLS: 128,
     PLAYER_MERGE_NEW_VER: true,
     PLAYER_AUTOSPLIT_SIZE: 0,
     PLAYER_SPLIT_CAP: 8,
-    PLAYER_MERGE_TIME: 5,
+    PLAYER_MERGE_TIME: 4,
     VIRUS_MONOTONE_POP: true,
     EJECT_SIZE: 38,
-    EJECT_LOSS: 38.2,
+    EJECT_LOSS: 39,
     EJECT_DELAY: 25,
-    BOTS: 25,
-    PELLET_COUNT: 1000,
-    PLAYER_SPAWN_SIZE: 1500,
+    BOTS: 50,
+    PELLET_COUNT: 5000,
+    PLAYER_SPAWN_SIZE: 1000,
     MAP_HW: 16384,
     MAP_HH: 16384
 });
