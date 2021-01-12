b056eba1b0cd058f44b44bb6b954848cf2e99c0c
Yuu6883 <okumura6883@gmail.com>
2021-01-11 16:14:44 -0800

    Updated demo

--- src/worker.js
+++ src/worker.js
@@ -5,18 +5,18 @@ const server = new Server();
 const engine = server.game.engine;
 
 engine.setOptions({
-    VIRUS_COUNT: 20,
+    VIRUS_COUNT: 250,
     PLAYER_MAX_CELLS: 128,
     PLAYER_MERGE_NEW_VER: true,
     PLAYER_AUTOSPLIT_SIZE: 0,
     PLAYER_SPLIT_CAP: 8,
-    PLAYER_MERGE_TIME: 5,
+    PLAYER_MERGE_TIME: 4,
     VIRUS_MONOTONE_POP: true,
     EJECT_SIZE: 38,
-    EJECT_LOSS: 38.2,
-    EJECT_DELAY: 25,
+    EJECT_LOSS: 38.4,
+    EJECT_DELAY: 50,
     BOTS: 100,
-    PELLET_COUNT: 1000,
+    PELLET_COUNT: 5000,
     PLAYER_SPAWN_SIZE: 1500,
     MAP_HW: 30000,
     MAP_HH: 30000
