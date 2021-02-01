059619532e297f5c57fb9039390239d196bef09b
Yuu6883 <okumura6883@gmail.com>
2021-01-31 19:08:28 -0800

    Updated run script

--- run.js
+++ run.js
@@ -1,19 +1,16 @@
 const fs = require("fs");
 const pm2 = require("pm2");
+const path = require("path");
 
 /** @type {pm2.StartOptions[]} */
 const DefaultProcesses = [
     {
         name: "Server1",
-        script: "./src/index.js",
-        cwd: __dirname,
-        max_memory_restart: "500M",
-        kill_timeout: 3000,
         env: {
             OGARX_MODE: "default/mega",
             OGARX_PORT: 3000,
             OGARX_SERVER: "Megasplit",
-            OGARX_ENDPOINT: "/mega",
+            OGARX_ENDPOINT: "mega",
             OGARX_TOKEN: process.env.OGARX_TOKEN
         }
     }
@@ -29,12 +26,34 @@ if (!fs.existsSync("config.json")) {
     config = require("./config.json");
 }
 
+const validateMode = mode => fs.existsSync(path.resolve(__dirname, "src", "modes", `${mode}.js`));
+
+/** @type {pm2.StartOptions[]} */
+const procToStart = [];
+
 for (const proc of config) {
-    console.log(`PM2 Process "${proc.name}" (${proc.env.OGARX_MODE}-${proc.env.OGARX_SERVER}) is mounted on ` +
-        `:${proc.env.OGARX_PORT || 443}${proc.env.OGARX_ENDPOINT || "/"}`);
+    proc.cwd = __dirname;
+    proc.script = "./src/index.js";
+    proc.max_memory_restart = "250M";
+    proc.kill_timeout = 3000;
+
+    if (validateMode(proc.env.OGARX_MODE)) {
+        procToStart.push(proc);
+    } else {
+        console.warn(`Unable to find mode "${proc.env.OGARX_MODE}", aborting process "${proc.name}"`);
+    }
 }
 
 pm2.connect(err => {
-    if (err) return console.error(err);
-    pm2.start(config, e => e ? console.error(e) : process.exit(0));
+    if (err) return console.error("Failed to connect to pm2", err);
+    const tasks = procToStart.map(proc => new Promise(res => {        
+        pm2.start(proc, e => {
+            if (e) console.error(e);
+            else  console.log(`PM2 Process "${proc.name}" ` +
+                `(${proc.env.OGARX_MODE}-${proc.env.OGARX_SERVER}) mounted on ` +
+                `:${proc.env.OGARX_PORT || 443}${proc.env.OGARX_ENDPOINT || "/"}`);
+            res();
+        });
+    }));
+    Promise.all(tasks).then(() => process.exit(0));
 });
